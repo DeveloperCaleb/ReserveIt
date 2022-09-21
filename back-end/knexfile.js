65d3d0f694008d14b2e21f09a51cf9010b5b0161
Caleb Jordan <calebjordan117@yahoo.com>
2022-09-20 23:22:48 -0500

    Created Reservation Form frontend, first migration, edited scripts in ./package.json

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -4,14 +4,14 @@
  * You will not need to make changes to this file.
  */
 
-require('dotenv').config();
+require("dotenv").config();
 const path = require("path");
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_DEVELOPMENT = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_TEST = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_PREVIEW = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://vmbtlsjm:VFbbrrMQNT9JKO0M8CMG_2op0AlQBEsM@jelani.db.elephantsql.com/vmbtlsjm",
+  DATABASE_URL_DEVELOPMENT = "postgres://dvzqtjtz:kYYkwaVngDvarUl11jEKHwxQ46IvBT5c@jelani.db.elephantsql.com/dvzqtjtz",
+  DATABASE_URL_TEST = "postgres://nvhcxeoa:WnqvVeaO2GI7Q2CO_gqAieblRaIctZmi@jelani.db.elephantsql.com/nvhcxeoa",
+  DATABASE_URL_PREVIEW = "postgres://ymfomwtt:RFGPNE5u3m99P33Kkntag-Uj-7SK7xNv@jelani.db.elephantsql.com/ymfomwtt",
   DEBUG,
 } = process.env;
 
---
+++ front-end/src/new-reservations/ReservationForm.js
@@ -0,0 +1,134 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+/**
+ * * - Important information
+ * ! - This isn't working
+ * ? - I have a question about this
+ * TODO - Needs completed
+ */
+
+function ReservationForm() {
+  /*
+  TODO create new form
+    first name
+    last name
+    mobile_number
+    res date
+    res time
+  TODO create on click handler
+  TODO create on change handler
+  */
+  const history = useHistory();
+
+  let initialFormState = {
+    first_name: "",
+    last_name: "",
+    mobile_number: "",
+    reservation_date: "",
+    reservation_time: "",
+    people: "",
+  };
+
+  const [formData, setFormData] = useState({ ...initialFormState });
+
+  const handleChange = ({ target }) => {
+    setFormData({
+      ...formData,
+      [target.name]: target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+    setFormData({ ...initialFormState });
+  };
+
+  return (
+    <div>
+      <form name="newReservation" onSubmit={handleSubmit}>
+        <h1>New Reservation</h1>
+        <label htmlFor="first_name">
+          First Name:
+          <input
+            id="first_name"
+            type="text"
+            name="first_name"
+            onChange={handleChange}
+            value={formData.first_name}
+            required
+          ></input>
+        </label>
+        <br />
+        <label htmlFor="last_name">
+          Last Name:
+          <input
+            id="last_name"
+            type="text"
+            name="last_name"
+            onChange={handleChange}
+            value={formData.last_name}
+            required
+          ></input>
+        </label>
+        <br />
+        <label htmlFor="mobile_number">
+          Mobile:
+          <input
+            id="mobile_number"
+            type="text"
+            name="mobile_number"
+            onChange={handleChange}
+            value={formData.mobile_number}
+            required
+          ></input>
+        </label>
+        <br />
+        <label htmlFor="reservation_date">
+          Reservation Date:
+          <input
+            type="date"
+            id="reservation_date"
+            name="reservation_date"
+            onChange={handleChange}
+            value={formData.reservation_date}
+            required
+          ></input>
+        </label>
+        <br />
+        <label htmlFor="reservation_time">
+          Reservation Time:
+          <input
+            type="time"
+            id="reservation_time"
+            name="reservation_time"
+            onChange={handleChange}
+            value={formData.reservation_time}
+            required
+          ></input>
+        </label>
+        <br />
+        <label htmlFor="people">
+          Party size:
+          <input
+            type="number"
+            id="people"
+            name="people"
+            onChange={handleChange}
+            value={formData.people}
+            min="1"
+            required
+          ></input>
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+        <button type="button" onClick={() => history.goBack()}>
+          Cancel
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default ReservationForm;
