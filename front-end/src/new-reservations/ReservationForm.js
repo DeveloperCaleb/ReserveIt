609ecee5b0318aed60d460b154b950a5ffabda17
Caleb Jordan <calebjordan117@yahoo.com>
2022-10-13 19:55:03 -0500

    reservation form shares a form component for new and edit page

--- front-end/src/new-reservations/ReservationForm.js
+++ front-end/src/new-reservations/ReservationForm.js
@@ -3,6 +3,7 @@ import { useHistory } from "react-router-dom";
 import { today } from "../utils/date-time";
 import axios from "axios";
 import ErrorAlert from "../layout/ErrorAlert";
+import ResForm from "../res-form/ResForm";
 
 function ReservationForm() {
   const history = useHistory();
@@ -97,95 +98,13 @@ function ReservationForm() {
   };
 
   return (
-    <div>
-      <form name="newReservation" onSubmit={handleSubmit}>
-        <h1>New Reservation</h1>
-        <label htmlFor="first_name">
-          First Name:
-          <input
-            id="first_name"
-            type="text"
-            name="first_name"
-            onChange={handleChange}
-            value={formData.first_name}
-            required
-          ></input>
-        </label>
-        <br />
-        <label htmlFor="last_name">
-          Last Name:
-          <input
-            id="last_name"
-            type="text"
-            name="last_name"
-            onChange={handleChange}
-            value={formData.last_name}
-            required
-          ></input>
-        </label>
-        <br />
-        <label htmlFor="mobile_number">
-          Mobile:
-          <input
-            id="mobile_number"
-            type="text"
-            name="mobile_number"
-            onChange={handleChange}
-            value={formData.mobile_number}
-            required
-          ></input>
-        </label>
-        <br />
-        <label htmlFor="reservation_date">
-          Reservation Date:
-          <input
-            type="date"
-            id="reservation_date"
-            name="reservation_date"
-            onChange={handleChange}
-            value={formData.reservation_date}
-            required
-          ></input>
-        </label>
-        <br />
-        <label htmlFor="reservation_time">
-          Reservation Time:
-          <input
-            type="time"
-            id="reservation_time"
-            name="reservation_time"
-            onChange={handleChange}
-            value={formData.reservation_time}
-            required
-          ></input>
-        </label>
-        <br />
-        <label htmlFor="people">
-          Party size:
-          <input
-            type="number"
-            id="people"
-            name="people"
-            onChange={handleChange}
-            value={formData.people}
-            min="1"
-            required
-          ></input>
-        </label>
-        <br />
-        <div>
-          {" "}
-          <button type="submit">Submit</button>
-          <button type="button" onClick={() => history.goBack()}>
-            Cancel
-          </button>
-        </div>
-      </form>
-      <p className={error !== "" ? "alert alert-danger" : ""}>
-        {error !== ""} {error}
-      </p>
-      <ErrorAlert error={postError} />
-    </div>
+    <ResForm
+      formData={formData}
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
+      error={error}
+      axiosError={postError}
+    />
   );
 }
 
